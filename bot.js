aeecf72dd075f230219897dec08d3e336a2fb25d
Vignesh P <vignesh.p1110@gmail.com>
2020-05-12 23:47:10 +0530

    Added sending randomized roles to DMs

--- bot.js
+++ bot.js
@@ -41,8 +41,8 @@ logger.level = 'debug';
 
 // Initialize Discord Bot
 let bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
+    token: auth.token,
+    autorun: true
 });
 bot.on('ready', function (evt) {
     logger.info('Connected');
@@ -51,14 +51,42 @@ bot.on('ready', function (evt) {
 });
 
 let boardState = false;
-let playerList = {}
-let messageIDmap = {}
-
-
-const dispNames = (playerList) => {
-    return Object.values(playerList).map((user, i) => `${i+1}---*${user}*`).join('\n');
+let playerList = {}// 
+let dummyPlayerList = {
+    '0444': 'adois',
+    '0333': 'deepak',
+    '0999': 'arvindh',
+    '0707': 'sandjae'
 }
 
+let messageIDmap = {};
+
+let fullDetailsOfPlayer = []; // {userId:'id', name:'name', role:'role'}
+
+const dispNames = (playerList) => Object.values(playerList)
+    .map((user, i) => `${i + 1} --- *${user}*`)
+    .join('\n')
+
+const randomGenRoles = (count) => {
+    let roleArray = ['H', 'L', 'L', 'L', 'F', 'L', 'F', 'L', 'F', 'L'];
+    roleArray.splice(count);
+    return roleArray.sort((a, b) => 0.5 - Math.random());
+}
+
+
+function sendRoles(playerList, channelID) {
+    const count = Object.keys(playerList).length
+    const roles = randomGenRoles(count); // ['F', 'L' , 'H' ]
+    fullDetailsOfPlayer = Object.keys(playerList).map((playerId, i) => ({ playerId, name: playerList[playerId], role: roles[i] }))
+    console.log(fullDetailsOfPlayer)
+    fullDetailsOfPlayer.map((player) => {
+        bot.sendMessage({
+            to: player.playerId,
+            message: `Your role is ***${player.role}***.`
+        })
+    }
+    )
+}
 
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
@@ -66,10 +94,10 @@ bot.on('message', function (user, userID, channelID, message, evt) {
     if (message.substring(0, 3) == 'sh!') {
         let args = message.substring(3).split(' ');
         let cmd = args[0];
-        
+
 
         args = args.splice(1);
-        switch(cmd) {
+        switch (cmd) {
             // sh!test
             case 'test':
                 bot.uploadFile({
@@ -77,9 +105,9 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                     file: './images/SecretHitler_icon25percent.png',
                     message: '>>> ***\tWelcome to Secret Hitler!***'
                 });
-            break;
+                break;
 
-            case 'open':                
+            case 'open':
                 /* bot.uploadFile({
                     to: channelID,
                     file: './images/SecretHitler_icon25percent.png',
@@ -91,22 +119,22 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                     })
                 }); */
 
-                let numofPlayers = 0;               
+                let numofPlayers = 0;
 
                 bot.sendMessage({
                     to: channelID,
                     embed: {
                         color: 1752220,
                         title: '**Players**',
-                        description: `A board has been opened. Please type sh!join if you wish to join the game.`    
+                        description: `A board has been opened. Please type sh!join if you wish to join the game.`
                     }
-                }, function(err, res) {
+                }, function (err, res) {
                     messageIDmap.openBoard = res.id;
                 });
 
                 boardState = true;
 
-            break;
+                break;
 
             case 'join':
                 if (boardState) {
@@ -116,8 +144,8 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                         messageID: messageIDmap.openBoard,
                         embed: {
                             color: 1752220,
-                            title: '**Players**',
-                            description: `A board has been opened. Please type sh!join if you wish to join the game. \n ${dispNames(playerList)}`    
+                            title: '**Players joining this game**',
+                            description: `A board has been opened. Please type sh!join if you wish to join the game. \n ${dispNames(playerList)}`
                         }
                     });
                 }
@@ -130,10 +158,10 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                 console.log(playerList);
 
 
-            break;
+                break;
 
             case 'begin':
-                if (boardState) {
+                if (boardState && Object.keys(playerList).length > 4 && Object.keys(playerList).length < 11) {
                     bot.sendMessage({
                         to: channelID,
                         message: dedent(
@@ -143,17 +171,48 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                     });
                 }
                 else {
+                    if (Object.keys(playerList).length < 5) {
+                        bot.sendMessage({
+                            to: channelID,
+                            message: 'Sorry, but the game requires atleast 5 players!'
+                        });
+                    }
+                    else if (Object.keys(playerList).length > 10) {
+                        bot.sendMessage({
+                            to: channelID,
+                            message: 'Sorry, too many players. Maximum player limit per game is 10!'
+                        });
+                    }
+                    else {
+                        bot.sendMessage({
+                            to: channelID,
+                            message: 'Board has not been opened yet. Please type sh!open to a game first.'
+                        });
+                    }
+                    //boardState = false;
+                }
+
+                break;
+
+            case 'begin-test':
+                if (boardState) {
                     bot.sendMessage({
                         to: channelID,
-                        message: 'Board has not been opened yet. Please type sh!open to a game first.'
+                        message: dedent(
+                            `*The year is 1932. The place is pre-WWII Germany. 
+                            In Secret Hitler, players are German politicians attempting to hold a fragile Liberal government together and stem the rising tide of Fascism. 
+                            Watch out though— there are secret Fascists among you, and one of them is the Secret Hitler.*`)
                     });
+                    playerList = { ...playerList, ...dummyPlayerList };
+                    sendRoles(playerList, channelID);
                 }
-                
-            break;
+                break;
+
+
 
 
 
             // Just add any case commands if you want to..
-         }
-     }
+        }
+    }
 });
