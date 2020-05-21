50df8b627f75e671c4910996726a6813ba61401d
Vignesh P <vignesh.p1110@gmail.com>
2020-05-21 19:17:59 +0530

    removing jsons and replacing with static_data.py

---
+++ Python/static_data.py
@@ -0,0 +1,69 @@
+
+
+colours = {
+    "DEFAULT": 0,
+    "AQUA": 1752220,
+    "GREEN": 3066993,
+    "BLUE": 3447003,
+    "PURPLE": 10181046,
+    "GOLD": 15844367,
+    "ORANGE": 15105570,
+    "RED": 15158332,
+    "GREY": 9807270,
+    "DARKER_GREY": 8359053,
+    "NAVY": 3426654,
+    "DARK_AQUA": 1146986,
+    "DARK_GREEN": 2067276,
+    "DARK_BLUE": 2123412,
+    "DARK_PURPLE": 7419530,
+    "DARK_GOLD": 12745742,
+    "DARK_ORANGE": 11027200,
+    "DARK_RED": 10038562,
+    "DARK_GREY": 9936031,
+    "LIGHT_GREY": 12370112,
+    "DARK_NAVY": 2899536,
+    "LUMINOUS_VIVID_PINK": 16580705,
+    "DARK_VIVID_PINK": 12320855,
+}
+
+images = {
+    "welcome.png" : "./images/Thumbnail.png",
+    "banner.jpg" : "./images/Banner.jpg",
+    "role.png" : {
+        "Facist" : [
+            "./images/Role_Facist0.png",
+            "./images/Role_Facist1.png",
+            "./images/Role_Facist2.png"
+        ],
+        "Hitler" : "./images/Role_Hitler.png",
+        "Liberal" : [
+            "./images/Role_Liberal0.png",
+            "./images/Role_Liberal1.png",
+            "./images/Role_Liberal2.png",
+            "./images/Role_Liberal3.png",
+            "./images/Role_Liberal4.png",
+            "./images/Role_Liberal5.png",
+        ]
+    },
+    "baseboard.png" : {
+        "5_6" :  "./images/Board1.png",
+        "7_8" :"./images/Board2.png",
+        "9_10" :"./images/Board3.png"
+    },
+    "policy.png" : {
+        "hidden" : "./images/Policy_Back.png",
+        "Facist" : "./images/Policy_Facist.png",
+        "Liberal" : "./images/Policy_Liberal.png"
+    },
+    "party.png" : {
+        "hidden" : "./images/PartyMembership.png",
+        "Facist" : "./images/PartyFacist.png",
+        "Liberal" : "./images/PartyLiberal.png"
+    },
+    "vote.png" : {
+        "Ja" : "./images/votJa.png",
+        "Nein" : "./images/votNein.png"
+    },
+    "logo.png" : "./images/SecretHitlerLogo.png",
+    "title.png" : "./images/plainTitle.png"
+}
