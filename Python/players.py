9a1e809028d2a6bf9d7e6fd9b46fb2ce8dae2cde
Jeswin Prince <jeswin_prince@trimble.com>
2020-05-22 08:25:40 +0530

    Moved send roles into board class

--- Python/players.py
+++ Python/players.py
@@ -1,18 +1,19 @@
 class Player:
-    def __init__(self, id: str, name: str, avatar: str, isbot: bool):
+    def __init__(self, id: str, name: str, avatar: str, isbot: bool, user=None):
         self.__id = id
         self.__name = name
         self.__avatar = avatar
         self.__isbot = isbot
         self.__role = None
         self.__rolePic = None
+        self.__user = user
 
     def __str__(self):
         return vars(self)
 
     @classmethod
     def from_Discord(cls, user):
-        return cls(user.id, user.name, user.avatar_url, user.bot)
+        return cls(user.id, user.name, user.avatar_url, user.bot, user)
 
     @property
     def id(self) -> str:
@@ -29,3 +30,23 @@ class Player:
     @property
     def isbot(self) -> bool:
         return self.__isbot
+
+    @property
+    def role(self) -> str:
+        return self.__role
+
+    @property
+    def rolePic(self) -> str:
+        return self.__rolePic
+
+    @role.setter
+    def role(self, role: str):
+        self.__role = role
+
+    @rolePic.setter
+    def rolePic(self, rolePic: str):
+        self.__rolePic = rolePic
+
+    async def send(self, file, embed):
+        if self.__user:
+            await self.__user.send(file=file, embed=embed)
