1488bffe5e45d03d2b6a78b0debcefa545b9f01a
Vignesh P <vignesh.p1110@gmail.com>
2020-05-23 02:45:26 +0530

    Added coordinate details for board in static_data

--- Python/test.py
+++ Python/test.py
@@ -1,21 +1,11 @@
 from PIL import Image
 
-liberalBoard = Image.open("./images/LiberalBoard.png")
-print(liberalBoard.size)
+board = Image.open("./images/Board1.png")
+print(board.size)
 
-facistBoard = Image.open("./images/FacistBoard3.png")
-print(facistBoard.size)
-""" facistBoard = Image.open("./images/FacistBoard2.png")
-print(facistBoard.size)
-facistBoard = Image.open("./images/FacistBoard3.png")
-print(facistBoard.size) """
+dot = Image.open("./images/Policy_Facist.png")
+print(dot.size)
 
-liberalBoard = liberalBoard.resize((1110, 364))
-# liberalBoard.show()
-
-facistBoard = facistBoard.resize((1110, 364))
-# facistBoard.show()
-boardImage = Image.new("RGBA", (1110, 728), "white")
-boardImage.paste(liberalBoard, (0, 0, 1110, 364))
-boardImage.paste(facistBoard, (0, 364, 1110, 728))
-boardImage.show()
+new = board.copy()
+new.paste(dot, (894, 456), dot)
+new.show()
